a72446aa8cbb925f67d8fdabb4d02c57d3ef3e51
Gaspiller <2574664155@qq.com>
2021-01-31 14:38:14 +0800

    1、个人资料页 2、个人资料修改页

--- kindergarten_be/user/views.py
+++ kindergarten_be/user/views.py
@@ -9,8 +9,15 @@ import hashlib
 
 class UserViews(View):
 
-    # def get(self, request):
-    #     return JsonResponse({'code': 200, 'msg': 'test'})
+    def get(self, request, username=None):
+        if username:
+            try:
+                user = UserProfile.objects.get(username=username)
+            except Exception as e:
+                result = {'code': 10102, 'error': '用户名错误'}
+                return JsonResponse(result)
+
+        return JsonResponse({'code': 200, 'msg': '测试'})
 
     def post(self, request):
         json_str = request.body
@@ -28,7 +35,7 @@ class UserViews(View):
 
         old_users = UserProfile.objects.filter(username=username)
         if old_users:
-            result = {'code': 10102, 'error': '用户名已存在'}
+            result = {'code': 10101, 'error': '用户名已存在'}
             return JsonResponse(result)
 
         p_m = hashlib.md5()
