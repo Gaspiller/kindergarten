c8a647b1a9f269f6ec686f09bc79f2b48118e768
Gaspiller <2574664155@qq.com>
2021-02-02 08:51:30 +0800

    html更改

--- kindergarten_be/article/models.py
+++ kindergarten_be/article/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=30, verbose_name='标题')
+    little_title = models.CharField(max_length=50, verbose_name='小标题')
+    content = models.CharField(max_length=3000, verbose_name='正文')
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'Article'
