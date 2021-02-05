b40fcbe084ff2b1e5beacec513fc7181a84bd824
Gaspiller <2574664155@qq.com>
2021-02-05 19:43:51 +0800

    user

---
+++ kindergarten_be/user/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+import random
+
+
+def default_sign():
+    signs = ['来吧，兄弟!', '快来吧，兄弟!']
+    return random.choice(signs)
+
+
+class UserProfile(models.Model):
+    username = models.CharField(max_length=12, verbose_name='用户名', primary_key=True)
+    nickname = models.CharField(max_length=30, verbose_name='昵称')
+    password = models.CharField(max_length=32)
+    email = models.EmailField()
+    phone = models.CharField(max_length=11)
+    sign = models.CharField(max_length=50, verbose_name='个人签名', default=default_sign)
+    info = models.CharField(max_length=150, verbose_name='个人简介', default='')
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+    coin = models.IntegerField(verbose_name="金币", default=0)
+
+    class Meta:
+        db_table = 'user_user'
